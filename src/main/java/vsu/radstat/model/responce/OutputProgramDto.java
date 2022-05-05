df054276cde9f1e46bb04685a3bdeb6fe4305a46
Элина Обухова <45429218+WriteWrote@users.noreply.github.com>
2022-05-05 19:53:47 +0300

    добавила аннотации на responce-dto

--- src/main/java/vsu/radstat/model/responce/OutputProgramDto.java
+++ src/main/java/vsu/radstat/model/responce/OutputProgramDto.java
@@ -1,5 +1,6 @@
 package vsu.radstat.model.responce;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 import vsu.radstat.model.input.addon.RecordDto;
@@ -9,12 +10,18 @@ import java.util.List;
 
 @Data
 public class OutputProgramDto {
+    @JsonProperty("length")
     @NotEmpty
     private Integer length;
+
+    @JsonProperty("genre_name")
     @NotEmpty
     private String genre;
+
+    @JsonProperty("date")
     @NotEmpty
     private LocalDateTime date;
+
     @NotEmpty
     List<RecordDto> records;
 }
